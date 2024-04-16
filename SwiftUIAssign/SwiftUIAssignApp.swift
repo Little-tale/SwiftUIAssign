713091144162cc19ac66506f909e99ab2a3779ce
judge_happily <you3192001@gmail.com>
2024-04-16 13:27:11 +0900

    Initial Commit

---
+++ SwiftUIAssign/SwiftUIAssignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAssignApp.swift
+//  SwiftUIAssign
+//
+//  Created by Jae hyung Kim on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAssignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
