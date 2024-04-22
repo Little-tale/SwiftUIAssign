d094ef77c0b6a2d375e1332c87bbeaf6e898ab39
judge_happily <you3192001@gmail.com>
2024-04-22 09:36:46 +0900

    Fetch: 모델 분리

--- SwiftUIAssign/JackFlixAssign/MyRandomImageView.swift
+++ SwiftUIAssign/JackFlixAssign/MyRandomImageView.swift
@@ -13,52 +13,12 @@ struct MyRandomImageView: View {
         NavigationView {
             ScrollView {
                 // 1. 섹션
-                VStack(alignment: .leading, content: {
-                    Text("첫번째 섹션")
-                        .font(.headline)
-                        .padding()
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 20) {
-                            ForEach(0..<20) {
-                                index in
-                                RendomImageView(cgSize: .init(width: 150, height: 200))
-                            }
-                        }
-                    }
-                    
-                })
-                VStack(alignment: .leading, content: {
-                    Text("두번째 섹션")
-                        .font(.headline)
-                        .padding()
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 20) {
-                            ForEach(0..<20) {
-                                index in
-                                RendomImageView(cgSize: .init(width: 150, height: 200))
-                            }
-                        }
-                    }
-                    
-                })
-                VStack(alignment: .leading, content: {
-                    Text("세번째 섹션")
-                        .font(.headline)
-                        .padding()
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 20) {
-                            ForEach(0..<20) {
-                                index in
-                                RendomImageView(cgSize: .init(width: 150, height: 200))
-                            }
-                        }
-                    }
-                    
-                })
+                RandomImageSection(sectionTitle: "첫번째 섹션", framsSize: .init(width: 170, height: 230))
+                RandomImageSection(sectionTitle: "두번째 섹션", framsSize: .init(width: 170, height: 230))
+                RandomImageSection(sectionTitle: "세번째 섹션", framsSize: .init(width: 170, height: 230))
             }.navigationTitle("My Random Image")
         }
     }
 }
+
+
---
+++ SwiftUIAssign/JackFlixAssign/RandomImageSection.swift
@@ -0,0 +1,32 @@
+//
+//  RandomImageSection.swift
+//  SwiftUIAssign
+//
+//  Created by Jae hyung Kim on 4/22/24.
+//
+
+import SwiftUI
+
+struct RandomImageSection: View {
+    
+    let sectionTitle: String
+    let framsSize: CGSize
+    
+    var body: some View {
+        VStack(alignment: .leading, content: {
+            Text(sectionTitle)
+                .font(.headline)
+                .padding()
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20) {
+                    ForEach(0..<20) {
+                        index in
+                        RendomImageView(cgSize: framsSize)
+                    }
+                }
+            }
+            
+        })
+    }
+}
